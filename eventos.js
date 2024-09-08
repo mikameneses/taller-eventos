0be5723bb1ee2548e7bc3b22e9617210338b31b7
mikameneses <mikameneses99@gmail.com>
2024-09-08 20:56:20 -0300

    Update eventos.js

--- eventos.js
+++ eventos.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
 
     const div = document.getElementById("myDiv");
+    const button = document.querySelector("#myDiv button");
   
     div.addEventListener("click", function () {
       alert("Hola! Soy el div");
