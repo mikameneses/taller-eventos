354a0274900dfc19d454a9ccacd511d8d6a0e047
Mikaela <mikameneses99@gmail.com>
2024-09-08 20:16:45 -0300

    event listener eventos.js

---
+++ eventos.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+    const div = document.getElementById("myDiv");
+  
+    div.addEventListener("click", function () {
+      alert("Hola! Soy el div");
+    });
+  });
